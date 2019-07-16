8971c9c052ec35cf35f80f82366e2bc413350bd2
DMYTRO BLUDOV <weltix@gmail.com>
2019-07-16 08:36:00 +0300

    Продолжать с TablePrintDemo1.java in package features

---
+++ Swing/src/com/weltix/java/tutorial/swing/features/TranslucentWindowDemo.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 15.07.2019, DMITRIY BLUDOV. All rights reserved.
+ */
+
+package com.weltix.java.tutorial.swing.features;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.GraphicsDevice.WindowTranslucency.TRANSLUCENT;
+
+public class TranslucentWindowDemo extends JFrame {
+    public TranslucentWindowDemo() {
+        super("TranslucentWindow");
+        setLayout(new GridBagLayout());
+
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add a sample button.
+        add(new JButton("I am a Button"));
+    }
+
+    public static void main(String[] args) {
+        // Determine if the GraphicsDevice supports translucency.
+        GraphicsEnvironment ge =
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        //If translucent windows aren't supported, exit.
+        if (!gd.isWindowTranslucencySupported(TRANSLUCENT)) {
+            System.err.println(
+                    "Translucency is not supported");
+            System.exit(0);
+        }
+
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        // Create the GUI on the event-dispatching thread
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                TranslucentWindowDemo tw = new TranslucentWindowDemo();
+
+                // Set the window to 55% opaque (45% translucent).
+                tw.setOpacity(0.55f);
+
+                // Display the window.
+                tw.setVisible(true);
+            }
+        });
+    }
+}
